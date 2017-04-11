18d2cbb64880fe9b39c8c11cc16eaffd1fdf7075
somenugget <some.nugget@gmail.com>
2017-04-11 22:18:22 +0300

    Try all inputs in benchmark.rb

--- benchmark.rb
+++ benchmark.rb
@@ -1,12 +1,31 @@
 require 'benchmark'
+require 'net/http'
 
 task = ARGV[0]
-file_path = "./#{task}/#{task}.rb"
+file_path = "./#{task}/#{task}"
+executable_file = "#{file_path}.rb"
+in_file = "#{file_path}.in"
+out_file = "#{file_path}.out"
 
-abort("#{file_path} not found") unless File.exist?(file_path)
+abort("#{executable_file} not found") unless File.exist?(executable_file)
 
 Benchmark.bm do |x|
-  x.report do
-    require file_path
+  (1..10).each do |n|
+    data_num = n.to_s.rjust(2, '0')
+    input_data = Net::HTTP.get(
+      '80.254.55.156',
+      "/#{task}/inputData/#{data_num}.in", '8888'
+    )
+
+    f = File.open(in_file, 'wb')
+    f.write(input_data)
+    f.close
+
+    x.report do
+      `ruby #{executable_file}`
+    end
+
+    # puts input_data
+    puts "#{data_num}.answer - #{File.read(out_file)}"
   end
 end
