f394ad93a5715c398e23cdb42bd06b8ec72616c5
somenugget <some.nugget@gmail.com>
2017-04-13 23:40:08 +0300

    Add hamstr task

--- hamstr/hamstr.rb
+++ hamstr/hamstr.rb
@@ -1,34 +1,32 @@
-
+require 'byebug'
 input_file = "#{File.dirname(__FILE__)}/hamstr.in"
-# output_file = "#{File.dirname(__FILE__)}/hamstr.out"
+output_file = "#{File.dirname(__FILE__)}/hamstr.out"
 
 file = File.open(input_file)
 file_content = file.read.split("\n")
 
-food = file_content.shift
-max_hamsters_count = file_content.shift
+food = file_content.shift.to_i
+max_hamsters_count = file_content.shift.to_i
 hamsters = file_content.map do |ham|
   hamster = ham.split ' '
-  { consumption: hamster[0], greed: hamster[1] }
+  { consumption: hamster[0].to_i, greed: hamster[1].to_i }
 end
-p hamsters
 
 def min_consumption_for_hamsters_count(hamsters, count)
-  count = hamsters.size if hamsters.size < count
-  lowest = []
-
-  hamsters.each do |hamster|
-    consumption = hamster[:consumption] + hamster[:greed] *
+  consumptions = hamsters.map do |hamster|
+    hamster[:consumption] + hamster[:greed] * (count - 1)
   end
+
+  consumptions.min(count).sum
 end
 
-p min_consumption_for_hamsters_count(hamsters, 2)
+hamsters_count = 0
+(1..max_hamsters_count).each do |h_count|
+  consumption = min_consumption_for_hamsters_count(hamsters, h_count)
+  break if consumption > food
+  hamsters_count = h_count
+end
 
-
-
-# food = file.readline.to_i
-# max_hamsters_count = file.readline.to_i
-
-# while(line = file.readline.to_f) do
-#   p line
-# end
+open(output_file, 'w') do |f|
+  f << "#{hamsters_count}\n"
+end
